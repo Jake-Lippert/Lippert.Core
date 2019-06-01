e19e6ea187b4ddf70ddc4249fc391cf229051a9c
Jake Lippert <Jake.Lippert+GitHub@outlook.com>
2019-06-01 10:44:30 -0400

    Creating a generic version of .Net's CompositeDisposable

---
+++ Lippert.Core.Tests/Collections/CompositeDisposableTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lippert.Core.Collections;
+using Moq;
+using NUnit.Framework;
+
+namespace Lippert.Core.Tests.Collections
+{
+	[TestFixture]
+	public class CompositeDisposableTests
+	{
+		[Test]
+		public void TestDisposesAllChildrenOnDispose()
+		{
+			//--Arrange
+			var count = new Random().Next(10, 100);
+			var disposableMocks = Enumerable.Range(0, count).Select(x => new Mock<IDisposableThing>()).ToList();
+			for (var i = 0; i < count; i++)
+			{
+				disposableMocks[i].Setup(x => x.DoThing())
+					.Returns(i);
+			}
+
+			//--Act
+			var results = new List<int>();
+			using (var composite = new CompositeDisposable<IDisposableThing>(disposableMocks.Select(x => x.Object)))
+			{
+				foreach (var part in (System.Collections.IEnumerable)composite)
+				{
+					results.Add((part as IDisposableThing).DoThing());
+				}
+				foreach (var disposable in composite)
+				{
+					results.Add(disposable.DoThing());
+				}
+			}
+
+			//--Assert
+			disposableMocks.ForEach(dm =>
+			{
+				dm.Verify(d => d.DoThing(), Times.Exactly(2));
+				dm.Verify(d => d.Dispose(), Times.Once());
+			});
+			for (var i = 0; i < count; i++)
+			{
+				Assert.AreEqual(i, results[i]);
+			}
+		}
+	}
+
+	public interface IDisposableThing : IDisposable
+	{
+		int DoThing();
+	}
+}
---
+++ Lippert.Core/Collections/CompositeDisposable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lippert.Core.Collections
+{
+	/// <summary>
+	/// A generic collection of IDisposables that all get disposed upon disposing of the container
+	/// </summary>
+	public sealed class CompositeDisposable<T> : IEnumerable<T>, IDisposable
+		where T : IDisposable
+	{
+		private readonly List<T> _disposables;
+
+		public CompositeDisposable(IEnumerable<T> disposables) => _disposables = disposables?.ToList() ?? new List<T>();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<T> GetEnumerator() => _disposables.GetEnumerator();
+		
+		public void Dispose()
+		{
+			_disposables.ForEach(d => d?.Dispose());
+			_disposables.Clear();
+		}
+	}
+}
